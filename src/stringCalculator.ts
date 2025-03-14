171df40bfa0766d7c17b4a2d039a07e91ac5ea03
Naazim Khan <knaazimkhan@gmail.com>
2025-03-14 17:58:08 +0400

    test: handle single number input

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,5 @@
 export const add = (numbers: string): number => {
   if (numbers === "") return 0;
 
-  return parseInt(numbers);
+  return parseInt(numbers, 10);
 };
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -4,4 +4,9 @@ describe("String Calculator", () => {
   test("returns 0 when input is an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("returns the number when input is a single number", () => {
+    expect(add("1")).toBe(1);
+  });
+
 });
